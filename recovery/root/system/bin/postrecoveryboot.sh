869a427192d267854656128c107f4d0999ab7c77
MD Raza <raza231198@gmail.com>
2021-09-28 21:12:48 +0530

    guamp: Import chipone, ilitek , nova_flash touchscreen modules
    Signed-off-by: MD Raza <raza231198@gmail.com>

--- recovery/root/system/bin/postrecoveryboot.sh
+++ recovery/root/system/bin/postrecoveryboot.sh
@@ -10,6 +10,9 @@ firmware_file=
 # Load TouchScreen Modules
 insmod $module_path/himax_v3_mmi_hx83102d.ko
 insmod $module_path/himax_v3_mmi.ko
+insmod $module_path/ili9882_mmi.ko
+insmod $module_path/chipone_tddi_mmi.ko
+insmod $module_path/nova_0flash_mmi.ko
 
 # Load Needed Modules
 insmod $module_path/sensors_class.ko
@@ -22,4 +25,12 @@ insmod $module_path/moto_f_usbnet.ko
 insmod $module_path/qpnp-power-on-mmi.ko
 insmod $module_path/qpnp_adaptive_charge.ko
 
-return 0
+# Fix up chipone
+chipone_fw_path=/vendor/firmware/ICNL9911.bin
+flash_path=/sys/chipone-tddi/cts_firmware
+
+sleep 2
+
+echo $chipone_fw_path > $flash_path/update_from_file
+
+exit 0
